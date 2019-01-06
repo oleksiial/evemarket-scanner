831bf45acf9870ae31dd1e738aaa23358d93b55c
oleksiial <oleksii.al@gmail.com>
2019-01-06 05:25:54 +0100

    margin calculating

--- lib/evemarket_scanner.ex
+++ lib/evemarket_scanner.ex
@@ -1,26 +1,37 @@
 defmodule EvemarketScanner do
-	alias EvemarketScanner.{Repo, Character, EveClient, Urls}
+	alias EvemarketScanner.{Repo, Character, EveClient, Urls, Type}
 
-  def wallet(character_name) do
+  def wallet(character_name \\ "Alex Prog") do
 		character = Repo.get_by!(Character, character_name: character_name)
 		EveClient.get!(character, Urls.wallet(character.character_id))
 	end
 
-	def orders(character_name) do
+	def orders(character_name \\ "Alex Prog") do
 		character = Repo.get_by!(Character, character_name: character_name)
 		EveClient.get!(character, Urls.orders(character.character_id))
 	end
 
-	def transactions(character_name) do
+	def transactions(character_name \\ "Alex Prog") do
 		character = Repo.get_by!(Character, character_name: character_name)
 		EveClient.get!(character, Urls.transactions(character.character_id))
 	end
 
-	def order_history(character_name, page \\ 1) do
+	def order_history(character_name \\ "Alex Prog", page \\ 1) do
 		character = Repo.get_by!(Character, character_name: character_name)
 		EveClient.get!(character, Urls.order_history(character.character_id, page))	
 	end
 
+	def type_orders(region_id, type_id, order_type \\ "all", page \\ 1) do
+		HTTPoison.get!(Urls.type_orders(region_id, type_id, order_type, page)).body |> Jason.decode!
+	end
+	def type_orders_margin(region_id, type_id) do
+		last_sell_order = hd type_orders(region_id, type_id, "sell") |> Enum.take(-1)
+		last_buy_order = hd type_orders(region_id, type_id, "buy") |> Enum.take(-1)
+		diff = last_sell_order["price"] * (1-0.025-0.010)  - last_buy_order["price"] * (1+0.003)
+		diff / (last_buy_order["price"] * (1+0.003)) * 100
+	end
+
+	def type_info(name), do: [Repo.get_by!(Type, name: name).type_id] |> types_info()
 	def types_info(type_ids) do
 		Enum.reduce type_ids, [], fn x, acc -> acc ++ [(HTTPoison.get!(Urls.type_info(x)).body |> Jason.decode!)] end
 	end
--- lib/evemarket_scanner/urls.ex
+++ lib/evemarket_scanner/urls.ex
@@ -3,13 +3,9 @@ defmodule EvemarketScanner.Urls do
 
 	def wallet(char_id), do: "https://esi.evetech.net/latest/characters/#{char_id}/wallet/"
 	def orders(char_id), do: "https://esi.evetech.net/latest/characters/#{char_id}/orders/"
-	def transactions(char_id) do
-		"https://esi.evetech.net/latest/characters/#{char_id}/wallet/transactions/"
-	end
-
-	def order_history(char_id, page \\ 1) do
-		"https://esi.evetech.net/latest/characters/#{char_id}/orders/history/?page=#{page}"
-	end
+	def transactions(char_id), do: "https://esi.evetech.net/latest/characters/#{char_id}/wallet/transactions/"
+	def type_orders(region_id, type_id, order_type \\ "all", page \\ 1), do: "https://esi.evetech.net/latest/markets/#{region_id}/orders/?order_type=#{order_type}&page=#{page}&type_id=#{type_id}"
+	def order_history(char_id, page \\ 1), do: "https://esi.evetech.net/latest/characters/#{char_id}/orders/history/?page=#{page}"
 
 	def type_info(type_id), do: "https://esi.evetech.net/latest/universe/types/#{type_id}?language=en-us"
 	def group_info(group_id), do: "https://esi.evetech.net/latest/universe/groups/#{group_id}?language=en-us"
